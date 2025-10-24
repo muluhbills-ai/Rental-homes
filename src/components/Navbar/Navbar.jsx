f1f3cc7c46763aa34b98eec3e2e062940b3cefbe
Muluh Bills <muluhbills@gmail.com>
2025-10-23 17:02:37 -0700

    Fix: correct css module filename casing for netlify

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
-import styles from "./NavBar.module.css";
+import styles from './Navbar.module.css';
+
 
 function Navbar() {
   const location = useLocation();
